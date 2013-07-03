6e419fa4fe4f931cfe76da7078a3daf3f4f7343f
divya <divya@vinsol.com>
2013-07-03 13:19:08 +0530

    review

--- exercise2/checkmax3.js
+++ exercise2/checkmax3.js
@@ -1,34 +1,32 @@
-var count = 0, selectedValue = [];
-
+var selectedValue = [];
 // function to check the limit on the number of checkboxes selected.if user selects more than three an alert message is displayed
 function limitCheck(val) {
-    var checkNone = document.getElementById("none");
-    if (checkNone.checked === true) {
-        checkNone.checked = false;       //to uncheck the "none" element
-    }
-    if (val.checked === true) {
-        // this loop adds the elements to the array if it is checked.only after checking the value of count.       
-        if (count >= 3) {
-            val.checked = false;
-            alert("Only three days can be selected.You have already selected " + selectedValue[0].value + " , " + selectedValue[1].value + " and "+ selectedValue[2].value);
-        } else {
-            count = count + 1;
-            selectedValue.push(val);
-        }
-    } else {
-        // if the user after checking the checkbox unchecks it.the element added to the array is removed and count is decremented.
+  var checkNone = document.getElementById("none");
+  if (checkNone.checked === true) {
+      checkNone.checked = false;       //to uncheck the "none" element
+  }
+  if (val.checked === true) {
+    // this loop adds the elements to the array if it is checked.only after checking the value of selectedValue.length.       
+    if (selectedValue.length >= 3) {
+      val.checked = false;
+      alert("Only three days can be selected.You have already selected " + selectedValue[0].value + " , " + selectedValue[1].value + " and " + selectedValue[2].value);
+    }else {
+        selectedValue.length = selectedValue.length;
+        selectedValue.push(val);
+     }
+  }
+  else {
+        // if the user after checking the checkbox unchecks it.the element added to the array is removed and selectedValue.length is decremented.
         var index = selectedValue.indexOf(val);
         selectedValue.splice(index, 1);
-        count--;
-    }
+  }
 }
 
 // this function unchecks all the chechboxes after selecting none.
 function selectNone() {
-    var uncheck = document.getElementsByName("days");
-    for (var j=0;j < uncheck.length;j++) {
-         uncheck[j].checked = false;
-    }
-    count = 0;
-    selectedValue = []; 
+  var uncheck = document.getElementsByName("days");
+  for (var j = 0;j < uncheck.length;j++) {
+    uncheck[j].checked = false;
+   } 
+  selectedValue = []; 
 }
