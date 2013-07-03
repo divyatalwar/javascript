822a6764488df300605e5fe88a6dd8ab83b93c7c
divya <divya@vinsol.com>
2013-07-03 12:19:55 +0530

    review

--- exercise1/check.js
+++ exercise1/check.js
@@ -1,5 +1,4 @@
 //a function to check or uncheck all checkboxes depending upon the boolean value passed
-
 function checkAllOrNone(val) {
     var checkbox = document.getElementsByName("colors"), i; 
     for (i = 0; i < checkbox.length; i++) {
