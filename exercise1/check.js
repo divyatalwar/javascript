7811de048f9532ce649ca3506c11b0eb5e146e74
divyatalwar <divya@vinsol.com>
2013-07-03 12:39:27 +0530

    Update check.js

--- exercise1/check.js
+++ exercise1/check.js
@@ -3,5 +3,6 @@ function checkAllOrNone(val) {
     var checkbox = document.getElementsByName("colors"), i; 
     for (i = 0; i < checkbox.length; i++) {
         checkbox[i].checked = val;
+        return false;
     }
 }
