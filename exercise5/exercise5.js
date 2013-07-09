f3e9318d2bcf83d7e9fd81f013344d1badb311e2
divya <divya@vinsol.com>
2013-07-09 11:50:43 +0530

    review

--- exercise5/exercise5.js
+++ exercise5/exercise5.js
@@ -1,41 +1,27 @@
-var formelement ={
-  register : document.getElementById("registrationform"),
-  login : document.getElementById("login"),
-  email : document.getElementById("email"),
-  name : document.getElementById("name"),
-  homepage : document.getElementById("homepage"),
+register = document.getElementById("registrationform");
+var formElement = {
   text_area : document.getElementById("text_area"),
   checkbox : document.getElementById("check"),
 };
+
 //adding event handler for submit button
-formelement.register.addEventListener("submit" ,formvalidation);
+register.addEventListener("submit" ,formvalidation);
+
 function formvalidation(e)
-{ // a variable count to check if any of the fields is invalid it should prevent the form from submitting else submit the form if it is still zero.
-  count = 0;
-  //validate login field
-  if (formelement.login.value=="") {
-    alert("login Id cant be left empty");
-    count += 1;
-  }
-  //validate email field
-  else if (formelement.email.value=="") {
-    alert("email field cant be left empty");
-    count += 1;
-  }
-  //validate name field
-  else if (formelement.name.value=="") {
-    alert("name cant be left empty");
-    count += 1;
-  }
-  //validate home page field
-  else if (formelement.homepage.value=="") {
-    alert("homepage cant be left empty");
-    count += 1;
+{ // a flag variable to check the validity of the fields
+  var flag = false;
+  var element = document.getElementsByClassName("inputvalue");
+  for (var i=0;i < element.length;i++) {
+    if(element[i].value == "") {
+      alert(element[i].id + " cant be left empty");
+      flag = true;
+      break;
+    }
   }
   //validate text area field
-  else if (formelement.text_area.value.length < 50) {
-    alert(" ABOUT ME information is not complete: should be minimum 50 characters");
-    count += 1;
+  if (formElement.text_area.value.length < 50 & flag == false) {
+    alert(" ABOUT ME information is not complete: it should be minimum 50 characters");
+    flag = true;
   }  
   //confirm checkbox notification
   if (check.checked) {
@@ -43,16 +29,16 @@ function formvalidation(e)
   } 
   else {
        textstring = "dont want";
-    }
-  if (count==0 ){
-    if (confirm("Are you sure you " + textstring + " to receive any notificiaction")) {
-      } 
-  else {
-     count+=1;
-       }
   }
-  //if any of the field is invalid(count incremented) prevent it from submitting. 
-  if (count>0)
+  if (! flag){
+    if (confirm("Are you sure you " + textstring + " to receive any notificiaction")) {
+    } 
+    else {
+     e.preventDefault();
+    }
+  }
+  //if any of the field is invalid prevent it from submitting. 
+  if (flag)
   {
   e.preventDefault();
   }  
