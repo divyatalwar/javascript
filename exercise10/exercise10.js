6434627e178b2e80462ada311b5e04ced9f2f5e8
divya <divya@vinsol.com>
2013-07-11 15:14:07 +0530

    issues resolved

--- exercise10/exercise10.js
+++ exercise10/exercise10.js
@@ -1,9 +1,10 @@
 // object oriented approach
-function validate(value) {
-  // form id 
+
+function NumberValidation(value) {
+  // obtaining the id of the form
   this.form = document.getElementById(value);
-  this.checkNumberValidation = function (e) {
-    var regexNumber = /^[\d]+$/;
+  this.checkNumber = function (e) {
+    var regexNumber = /^(([\-]{1})?[\d]+(\.[\d]+)?)$/;
     if(regexNumber.test(document.getElementById("number").value)) {
       document.getElementById("result").value = 'true';
       alert("input is a valid number");
@@ -14,7 +15,7 @@ function validate(value) {
       alert("input is not a valid number");
     }
   }
-  this.form.addEventListener("submit", this.checkNumberValidation ,false);
+  this.form.addEventListener("submit", this.checkNumber , false);
 }
 
-var numericTest = new validate('formTest');
+var numericTest = new NumberValidation('formTest');
