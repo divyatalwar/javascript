3b0b17741ad197745b5ed70022a04eb9abd17722
divya <divya@vinsol.com>
2013-07-11 15:29:00 +0530

    issues resolved

--- exercise11/exercise11.js
+++ exercise11/exercise11.js
@@ -1,11 +1,12 @@
 // object oriented approach
-function validate(value) {
-  // form id 
+function URLValidation(value) {
+  // obtaining the form id 
   this.form = document.getElementById(value);
-  this.checkURLValidation = function (e) {
-  var regexUrl = /^((http|https|ftp):\/\/)?(w{3}\.)?((\w+\.)*)([a-zA-Z]+\.[a-z]{2,4})([/])?([\w%-]+)?([/?][\w%.])?([\w\-\.,@?^=%&amp;:~\+#]*)$/i;
-   url = document.getElementById("url").value;
-   if (regexUrl.test(url)) {
+  this.checkURL = function (e) {
+  regexUrl = /^((http|https|ftp):\/\/)?(w{3}\.)?((\w+\.)*)([a-zA-Z]+\.[a-z]{2,4})([/])?([\w%-]+)?([/?][\w%.])?([\w\-\.,@?^=%&amp;:~\+#]*)$/i;
+  url = document.getElementById("url").value;
+  //testing if the URL matches the above pattern 
+  if (regexUrl.test(url)) {
     domain = RegExp.$6;
     subDomain = RegExp.$4.replace(/\.$/,"");
       if (subDomain === "") {
@@ -21,6 +22,7 @@ function validate(value) {
       e.preventDefault();
     }
   }
-  this.form.addEventListener("submit", this.checkURLValidation ,false);
+  //adding an event listener for the submit button
+  this.form.addEventListener("submit", this.checkURL , false);
 }
-var URLTest = new validate('formTest');
+var URLTest = new URLValidation('formTest');
